fa7905df3eb6a4cee6ef4da46cbc607585d39d48
Delphine-C <corneil.delphine@gmail.com>
2019-03-16 18:41:31 +0100

    Variable in infos page

--- src/Controller/InfosController.php
+++ src/Controller/InfosController.php
@@ -13,11 +13,28 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class InfosController extends AbstractController
 {
+    private $annual_membership;
+
+    private $sessions_card;
+
+    private $annual_subscription;
+
+    public function __construct()
+    {
+        $this->annual_membership = getenv('annual_membership');
+        $this->sessions_card = getenv('sessions_card');
+        $this->annual_subscription = getenv('annual_subscription');
+    }
+
     /**
      * @Route("/infos", name="infospage")
      */
     public function getInfosPage()
     {
-        return $this->render('infos.html.twig');
+        return $this->render('infos.html.twig', [
+            'annual_membership' => $this->annual_membership,
+            'sessions_card' => $this->sessions_card,
+            'annual_subscription' => $this->annual_subscription,
+        ]);
     }
 }
